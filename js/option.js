92190e8e8f7d0c2301bb5b358cb9d3a7db97efa5
JB Profile <cool508@naver.com>
2021-04-06 23:30:46 +0900

    Add fullpage.js  in content.html

---
+++ js/option.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage',{
+  licensekey : 'OPEN-SOURCE-GPLV3-LICENSE',
+  autoScrolling:true,
+  scrollHorizontally: true,
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows:false
+});
+fullpage_api.setAllowScrolling(true);   
